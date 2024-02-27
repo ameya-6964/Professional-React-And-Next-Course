d61e25b222f2a69fa918029c0fc13aa59c46a9af
ameya-6964 <belvalkar7@gmail.com>
2024-02-27 14:29:13 +0530

    Learned JSX And Diffrence Between Normal HTMl and Jsx

--- beginner-react-javascript/src/App.jsx
+++ beginner-react-javascript/src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
 
+{
+  /* <div className="container">
+      <h1>Very Cool</h1>
+    </div>
+ */
+}
+// Above JSX Text Will Be Converted Into Below
+
+/* 
+React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("h1", null, "Very Cool")
+);
+ */
 function App() {
-  return <p> Very Cool</p>;
+  return (
+    <div className="container">
+      <h1 onClick={() => console.log("test")}>Very Cool</h1>
+    </div>
+  );
 }
 
 export default App;
